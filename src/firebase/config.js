bb468009db6990d1ebd0037f676a0beb060a48a8
tseitz <tdseitz10@gmail.com>
2020-06-12 20:15:38 -0500

    Add back some private stuff. Hope I got it

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default {
+  apiKey: process.env.FIRE_API_KEY,
+  authDomain: `${process.env.FIRE_PROJ_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.FIRE_PROJ_ID}.firebaseio.com`,
+  projectId: process.env.FIRE_PROJ_ID,
+  storageBucket: `${process.env.FIRE_PROJ_ID}.appspot.com`,
+  messagingSenderId: process.env.FIRE_SENDER_ID,
+  appId: process.env.FIRE_APP_ID,
+  messagingSenderId: `G-${process.env.FIRE_MEASUREMENT_ID}`,
+}
